1168278b2fff8def008f405d31d1da0ac48a9e0a
Heba10 <hebe@yahoo.com>
2020-04-27 02:03:07 +0200

    finsh Authorization on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
     http_basic_authenticate_with name: "heba", password: "123456", only: :destroy
 
     def create
+      @comment.user_id=current_user.id
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
